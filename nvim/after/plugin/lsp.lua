11411b478ceec374bd322997f54bc5abf501241f
shindexro <shindexro@gmail.com>
2023-12-03 23:35:22 +0000

    Stop auto installing LSP

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -6,13 +6,15 @@ lsp_zero.on_attach(function(client, bufnr)
   lsp_zero.default_keymaps({buffer = bufnr})
 end)
 
-require('mason').setup({})
-require('mason-lspconfig').setup({
-    ensure_installed = {'cmake', 'clangd'},
-    handlers = {
-        lsp_zero.default_setup,
-    }
-})
+
+--- Auto install LSP
+-- require('mason').setup({})
+-- require('mason-lspconfig').setup({
+--     ensure_installed = {'cmake', 'clangd'},
+--     handlers = {
+--         lsp_zero.default_setup,
+--     }
+-- })
 
 
 
