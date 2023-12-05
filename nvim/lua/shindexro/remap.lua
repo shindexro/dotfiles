9bb653509f886219215c44e0982db2e763ec176c
shindexro <shindexro@gmail.com>
2023-12-05 23:11:05 +0000

    Add vim keymaps

--- nvim/lua/shindexro/remap.lua
+++ nvim/lua/shindexro/remap.lua
@@ -1,4 +1,13 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Navigate between quickfix items
+vim.keymap.set("n", "<leader>h", "<cmd>cnext<CR>zz", { desc = "Forward qfixlist" })
+vim.keymap.set("n", "<leader>;", "<cmd>cprev<CR>zz", { desc = "Backward qfixlist" })
 
+-- Replace word under cursor across entire buffer
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+  { desc = "Replace word under cursor" })
+
+-- Make current file executable
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true, desc = "Make current file executable" })
