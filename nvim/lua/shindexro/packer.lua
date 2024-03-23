9ac049ea7283966c1942528710c4856c272f6c74
shindexro <shindexro@gmail.com>
2024-03-23 15:17:31 +0000

    Add comment about updating packer config

--- nvim/lua/shindexro/packer.lua
+++ nvim/lua/shindexro/packer.lua
@@ -1,4 +1,5 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
+-- execute :so, :PackerSync after updating this config
 
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
